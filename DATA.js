0bdc8c586cef47e1edb0b6fc817a6968be7ab4a1
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-10-07 23:57:53 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,140 @@
+export const item = {
+    "id": "060b7202-5419-460a-b1e2-5889a7cabec1",
+    "home_feed": [
+        {
+            "id": "2d404572-39c5-472f-9a79-c6c8a261b332",
+            "image_carousal": [
+                {
+                    "id": "7bf48d06-294e-4451-804b-d6bf94b388c4",
+                    "art_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/201.png"
+                },
+                {
+                    "id": "716f32ba-6cdb-4ed0-9fdd-99ea1abdb4eb",
+                    "art_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/18.png"
+                },
+                {
+                    "id": "e9c1aea9-420c-410b-ab39-460d02dd13bc",
+                    "art_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/28.png"
+                },
+                {
+                    "id": "9ab5bc62-c5b4-47fe-b36f-2d6f50897d30",
+                    "art_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/330.png"
+                },
+                {
+                    "id": "d6cc21e4-eb83-4721-b8f8-00171b2d2d8a",
+                    "art_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/34.png"
+                }
+            ],
+            "art_caption": "cause",
+            "art_description": "Top seven year. Home beat by trouble best firm. Figure son green issue compare contain rule."
+        },
+        {
+            "id": "a5d2ed0c-e7b3-442d-88c2-85749018b1d7",
+            "image_carousal": [
+                {
+                    "id": "28bd7221-8d07-458d-b50e-2ae1bdefa3b0",
+                    "art_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/215.png"
+                },
+                {
+                    "id": "a0bd8817-5921-4489-bb67-08dfd1a18a03",
+                    "art_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/100.png"
+                },
+                {
+                    "id": "64c6f496-8ad8-48ca-a378-77d8327652d5",
+                    "art_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/234.png"
+                },
+                {
+                    "id": "86f346b3-d6a4-47ef-81a6-4e25709e2600",
+                    "art_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/363.png"
+                },
+                {
+                    "id": "32363db4-c418-45fd-bc58-86e006260c5e",
+                    "art_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/263.png"
+                }
+            ],
+            "art_caption": "war",
+            "art_description": "Animal nice population true if poor never."
+        },
+        {
+            "id": "a26194e7-7ecc-45e6-9504-47b25e061f2e",
+            "image_carousal": [
+                {
+                    "id": "f9f1824f-72d4-441d-a02d-4a4a4d8bc23d",
+                    "art_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/94.png"
+                },
+                {
+                    "id": "019531f4-9667-4b23-8e38-7f2c68d37787",
+                    "art_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/83.png"
+                },
+                {
+                    "id": "a0aa2ec6-5642-4acc-9082-4d386a58ede8",
+                    "art_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/43.png"
+                },
+                {
+                    "id": "cbf27f2b-675e-4e5e-ada9-255afa95cf10",
+                    "art_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/67.png"
+                },
+                {
+                    "id": "ad8176ac-0a0e-451b-811b-c2a764733a0f",
+                    "art_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/282.png"
+                }
+            ],
+            "art_caption": "board",
+            "art_description": "Far concern coach start. Side effect image war gas consider. Still authority no agency network theory."
+        },
+        {
+            "id": "73847a2b-10d9-4da5-911f-b23ed2f09b54",
+            "image_carousal": [
+                {
+                    "id": "1b2f7268-b6ce-4f57-9ccd-f9521f874caa",
+                    "art_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/265.png"
+                },
+                {
+                    "id": "9f4bda82-9335-43ab-ac46-96fba9469107",
+                    "art_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/247.png"
+                },
+                {
+                    "id": "80aeb4e9-f04b-403a-93b1-7b7eebaf2e25",
+                    "art_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/85.png"
+                },
+                {
+                    "id": "d232e7a9-7245-4baf-9995-f489dcc3bdbd",
+                    "art_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/34.png"
+                },
+                {
+                    "id": "e647cf90-fd27-419d-a5ef-e7438eb8e6d0",
+                    "art_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/194.png"
+                }
+            ],
+            "art_caption": "fine",
+            "art_description": "Political better action science test toward. Describe first tell challenge. Someone ready who together population."
+        },
+        {
+            "id": "8af4244d-f8c2-46e0-8c97-d9defe4301a6",
+            "image_carousal": [
+                {
+                    "id": "07b57003-9c54-4f00-b63c-16de76d803fc",
+                    "art_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/329.png"
+                },
+                {
+                    "id": "b7cc4056-c637-403d-a67f-b27c07a92fb8",
+                    "art_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/117.png"
+                },
+                {
+                    "id": "dda4f811-4e42-4b8f-a632-4e6aeccb15d9",
+                    "art_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/22.png"
+                },
+                {
+                    "id": "00461225-d24e-4243-8b15-0ca22b11d930",
+                    "art_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/305.png"
+                },
+                {
+                    "id": "1427ee0a-fa68-41a9-b5ee-0abe26651a87",
+                    "art_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/187.png"
+                }
+            ],
+            "art_caption": "thing",
+            "art_description": "Knowledge second in want economy off task. Road card exist provide. Teacher college war field you prove deal."
+        }
+    ]
+}
